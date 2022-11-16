abb10d8a26eae6eada1c43cdd6421dd301d349b0
Narmin Molina <narminmolina@gmail.com>
2022-11-15 21:21:48 -0500

    Updates

---
+++ precache-manifest.4b53c9f2c33e78286612730651056783.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "40bf3f435864ac6e2975",
+    "url": "/Robots-React/static/css/main.a3d4b5c5.chunk.css"
+  },
+  {
+    "revision": "40bf3f435864ac6e2975",
+    "url": "/Robots-React/static/js/main.40bf3f43.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/Robots-React/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "8105882738ed02165816",
+    "url": "/Robots-React/static/css/2.c4ee51e7.chunk.css"
+  },
+  {
+    "revision": "8105882738ed02165816",
+    "url": "/Robots-React/static/js/2.81058827.chunk.js"
+  },
+  {
+    "revision": "3a0acdd6796048df1ed3",
+    "url": "/Robots-React/static/js/runtime~main.3a0acdd6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots-React/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9b43f600e157ea4382c1b48baf9105a7",
+    "url": "/Robots-React/index.html"
+  }
+];
